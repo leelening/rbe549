ff62939a356413bd73bf1ac0ad36184a244423f2
lening <lli4@wpi.edu>
2022-08-28 16:28:57 -0400

    update

--- YourDirectoryID_p1/Phase2/Code/Network/Network.py
+++ YourDirectoryID_p1/Phase2/Code/Network/Network.py
@@ -29,9 +29,30 @@ class HomographyModel(nn.Model):
         :type output_size: int
         """
         super().__init__()
+        # Spatial transformer localization-network
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+        )
         #############################
         # Fill your network initialization of choice here!
         #############################
+
+    # Spatial transformer network forward function
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 3 * 3)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
 
     def forward(self, input_img):
         """
